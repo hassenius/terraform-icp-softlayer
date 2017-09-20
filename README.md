f025bb3f8ec055cb500744bfc80aa4a3ea038ad0
Hans Kristian Moen <hans.kristian.moen@gmail.com>
2017-09-20 13:41:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Terraform ICP SoftLayer
+
+These TerraForm example configurations uses the [SoftLayer Provider](https://github.com/softlayer/terraform-provider-softlayer) to provision servers in SoftLayer
+and [TerraForm Module ICP Deploy](https://github.com/ibm-cloud-architecture/terraform-module-icp-deploy) to deploy [IBM Cloud Private](https://www.ibm.com/cloud-computing/products/ibm-cloud-private/) on them.
+
+
+### Pre-requisits
+
+* These examples use the SoftLayer Provider available from [Github](https://github.com/softlayer/terraform-provider-softlayer) rather than the older provider that currently ships with TerraForm.
+  Follow the [Installation instructions on the SoftLayer Github Page](https://github.com/softlayer/terraform-provider-softlayer#install)
+* You'll need a SoftLayer API key. If you don't already have them at hand, follow the [instructions on IBM KnowledgeLayer](https://knowledgelayer.softlayer.com/procedure/retrieve-your-api-key) to retrieve or generate them
+
---
+++ vms_smallcluster_icp-ce/instances.tf
@@ -0,0 +1,110 @@
+provider "softlayer" {
+    username = "${var.sl_username}"
+    api_key = "${var.sl_api_key}"
+}
+
+data "softlayer_ssh_key" "public_key" {
+  label = "${var.key_name}"
+}
+
+resource "softlayer_virtual_guest" "icpmaster" {
+    count       = "${var.master["nodes"]}"
+    
+    datacenter  = "${var.datacenter}"
+    domain      = "${var.domain}"
+    hostname    = "${format("${lower(var.instance_name)}-master%01d", count.index + 1) }"
+    
+    os_reference_code = "UBUNTU_16_64"
+
+    cores       = "${var.master["cpu_cores"]}"
+    memory      = "${var.master["memory"]}"
+    disks       = ["${var.master["disk_size"]}"]
+    local_disk  = "${var.master["local_disk"]}"
+    network_speed         = "${var.master["network_speed"]}"
+    hourly_billing        = "${var.master["hourly_billing"]}"
+    private_network_only  = "${var.master["private_network_only"]}"
+
+    user_metadata = "{\"value\":\"newvalue\"}"
+
+    ssh_key_ids = ["${data.softlayer_ssh_key.public_key.id}"]
+}
+
+resource "softlayer_virtual_guest" "icpworker" {
+    count       = "${var.worker["nodes"]}"
+
+    datacenter  = "${var.datacenter}"
+    domain      = "${var.domain}"
+    hostname    = "${format("${lower(var.instance_name)}-worker%01d", count.index + 1) }"
+
+    os_reference_code = "UBUNTU_16_64"
+    
+    cores       = "${var.worker["cpu_cores"]}"
+    memory      = "${var.worker["memory"]}"
+    disks       = ["${var.worker["disk_size"]}"]
+    local_disk  = "${var.worker["local_disk"]}"
+    network_speed         = "${var.worker["network_speed"]}"
+    hourly_billing        = "${var.worker["hourly_billing"]}"
+    private_network_only  = "${var.worker["private_network_only"]}"
+    
+    user_metadata = "{\"value\":\"newvalue\"}"
+    
+    ssh_key_ids = ["${data.softlayer_ssh_key.public_key.id}"]
+}
+
+resource "softlayer_virtual_guest" "icpproxy" {
+    count       = "${var.proxy["nodes"]}"
+
+    datacenter  = "${var.datacenter}"
+    domain      = "${var.domain}"
+    hostname    = "${format("${lower(var.instance_name)}-proxy%01d", count.index + 1) }"
+
+    os_reference_code = "UBUNTU_16_64"
+    
+    cores       = "${var.proxy["cpu_cores"]}"
+    memory      = "${var.proxy["memory"]}"
+    disks       = ["${var.proxy["disk_size"]}"]
+    local_disk  = "${var.proxy["local_disk"]}"
+    network_speed         = "${var.proxy["network_speed"]}"
+    hourly_billing        = "${var.proxy["hourly_billing"]}"
+    private_network_only  = "${var.proxy["private_network_only"]}"
+    
+    user_metadata = "{\"value\":\"newvalue\"}"
+    
+    ssh_key_ids = ["${data.softlayer_ssh_key.public_key.id}"]
+}
+
+module "icpprovision" {
+    source = "github.com/ibm-cloud-architecture/terraform-module-icp-deploy"
+    
+    icp-master = ["${softlayer_virtual_guest.icpmaster.ipv4_address}"]
+    icp-worker = ["${softlayer_virtual_guest.icpworker.*.ipv4_address}"]
+    icp-proxy = ["${softlayer_virtual_guest.icpproxy.*.ipv4_address}"]
+    
+    #icp-version = "2.1.0-beta-1"
+    icp-version = "1.2.0"
+
+    /* Workaround for terraform issue #10857
+     When this is fixed, we can work this out autmatically */
+
+    cluster_size  = "${var.master["nodes"] + var.worker["nodes"] + var.proxy["nodes"]}"
+
+    # Because SoftLayer private network uses 10.0.0.0/8 range, 
+    # we will override default ICP network configuration 
+    # to be sure to avoid conflict
+    icp_configuration = {
+      "network_cidr"              = "192.168.0.0/16"
+      "service_cluster_ip_range"  = "172.16.0.1/24"
+    }
+
+    # We will let terraform generate a new ssh keypair 
+    # for boot master to communicate with worker and proxy nodes
+    # during ICP deployment
+    generate_key = true
+    
+    # SSH user and key for terraform to connect to newly created SoftLayer resources
+    # ssh_key is the private key corresponding to the public keyname specified in var.key_name
+    ssh_user  = "root"
+    ssh_key   = "~/.ssh/id_rsa"
+    
+} 
+
